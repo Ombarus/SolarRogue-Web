5fe28a8732485d55974316ae84855e1aaf6c89a1
guillaume.bouvier <guillaume.bouvier@gameloft.com>
2020-02-05 12:04:09 +0900

    more cleanup, changed color scheme, started organizing & renaming sections

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {  
-  var modals = ['#privacydicedm', '#portfolioModal-DiceDM'];
-  if (window.location.pathname.includes("dicedmfree.html")) {
-	   $("#portfolioModal-DiceDM").modal();
-  }
+  var modals = ['#privacydicedm'];
   if (window.location.hash && ~modals.indexOf(window.location.hash)) {
      $(window.location.hash).modal();
   }
